ea4dca259383ad938106420cf4c9962444623764
DESKTOP-RIMIB5Q\jpribio <jaime.p.r023@gmail.com>
2025-02-10 14:27:26 +0100

    modificacion para angular

--- src/main/java/edu/proyectoFinalAPI/controlador/controladorApi.java
+++ src/main/java/edu/proyectoFinalAPI/controlador/controladorApi.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,6 +34,7 @@ import jakarta.ws.rs.core.MediaType;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class controladorApi {
 
