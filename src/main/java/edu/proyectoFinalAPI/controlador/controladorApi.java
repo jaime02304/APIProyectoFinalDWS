d637cbf1358588e68ecf24f65a0ebd4466bbe20b
DESKTOP-2U1CBE1\vspc <csi23-jpribio@colegioaltair.net>
2025-03-01 16:57:02 +0100

    Para angular

--- src/main/java/edu/proyectoFinalAPI/controlador/controladorApi.java
+++ src/main/java/edu/proyectoFinalAPI/controlador/controladorApi.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,6 +40,7 @@ import jakarta.ws.rs.core.MediaType;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class controladorApi {
 
